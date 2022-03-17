8e592594846064c79b00ccea01c68c82b9f6955c
노연주 <yjnoh1016@naver.com>
2022-03-17 20:15:22 +0900

    [UPDATE] Gradient

--- Tutorial/Tutorial3/Tutorial3/ContentView.swift
+++ Tutorial/Tutorial3/Tutorial3/ContentView.swift
@@ -11,6 +11,11 @@ struct ContentView: View {
     var body: some View {
         ZStack{
             Color.yellow.edgesIgnoringSafeArea(.all)
+            RadialGradient(gradient: Gradient(colors:[.white, .black]), center: .center, startRadius: 310, endRadius: 100)
+            AngularGradient(gradient: Gradient(colors:[.red,.yellow,.blue]), center: .center)
+            Color.red.frame(width:300,height: 300)
+            Color(red: 0.1, green: 0.2, blue: 0.3).frame(width:00,height:2000)
+            Color.blue.frame(width:100,height: 100)
         }
     }
 }
