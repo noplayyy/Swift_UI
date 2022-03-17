dffce0250e88784e70f1f20c201a7d827b5b940b
노연주 <yjnoh1016@naver.com>
2022-03-17 20:22:40 +0900

    [UPDATE] 예제 클리어

--- Tutorial/Tutorial3/Tutorial3/ContentView.swift
+++ Tutorial/Tutorial3/Tutorial3/ContentView.swift
@@ -10,12 +10,33 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack{
-            Color.yellow.edgesIgnoringSafeArea(.all)
-            RadialGradient(gradient: Gradient(colors:[.white, .black]), center: .center, startRadius: 310, endRadius: 100)
-            AngularGradient(gradient: Gradient(colors:[.red,.yellow,.blue]), center: .center)
-            Color.red.frame(width:300,height: 300)
-            Color(red: 0.1, green: 0.2, blue: 0.3).frame(width:00,height:2000)
-            Color.blue.frame(width:100,height: 100)
+            AngularGradient(gradient: Gradient(colors:[.red, .orange,.yellow,.green, .blue, .purple]), center: .center).edgesIgnoringSafeArea(.all)
+            VStack{
+                HStack{
+                    Text("11시")
+                    Spacer()
+                    Text("12시")
+                    Spacer()
+                    Text("1시")
+                }
+                Spacer()
+                HStack{
+                    Text("9시")
+                    Spacer()
+                    Text("중앙")
+                    Spacer()
+                    Text("3시")
+                }
+                Spacer()
+                HStack{
+                    Text("7시")
+                    Spacer()
+                    Text("6시")
+                    Spacer()
+                    Text("5시")
+                }
+            }
+            
         }
     }
 }
