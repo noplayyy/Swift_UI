4f4e62b8cb68f71a9eb6a4b6982fe61287349e5b
노연주 <yjnoh1016@naver.com>
2022-03-24 17:04:48 +0900

    [CREATE] Tutorial

---
+++ Tutorial/tutorial4/tutorial4/tutorial4App.swift
@@ -0,0 +1,17 @@
+//
+//  Tutorial4App.swift
+//  Tutorial4
+//
+//  Created by 노연주 on 2022/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Tutorial4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
